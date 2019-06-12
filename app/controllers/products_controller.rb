bfc02617462e514edd226e9186af54acc9b1c429
Nilesh <nsharma@grepruby.io>
2019-06-12 19:05:43 +0530

    shop Owener and product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,24 @@
 class ProductsController < ApplicationController
  
   def index
+    @products = Product.all
+    render @products
   end
   
   def new
-  end  
+    @product = Product.new
+    shop_owner = ShopOwner.find_by_user_id(current_user.id)
+    session[:shop_id] = shop_owner.shop_id
+    session[:shop_owner_id] = shop_owner.id
+  end
+
+  def create
+    @shop_owner = ShopOwner.find_by_user_id(current_user.id)
+    render plain: params[:product].inspect
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :quantity)
+  end
 end
